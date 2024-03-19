0f58debbdb608f0d22c06870558e056cbf8c1a7e
Johnny32id <johnnyhandsome3232@gmail.com>
2024-03-19 10:35:53 +0300

    added parse func

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,16 +1,23 @@
+#!/usr/bin/env python3
 import argparse
 import json
-parser = argparse.ArgumentParser(
-    description="Compares two configuration files and shows a difference.")
-parser.add_argument("-f", "--format", type=str,
-                    help="set format of outputs")
-parser.add_argument("first_file")
-parser.add_argument("second_file")
-args = parser.parse_args()
 
 
-def generate_diff(file_1, file_2):
-    unique_keys = set(file_1) | set(file_2)
+def parse():
+    parser = argparse.ArgumentParser(
+        description="Compares two configuration files and shows a difference.")
+    parser.add_argument("-f", "--format", type=str,
+                        help="set format of outputs")
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+    args = parser.parse_args()
+    return {'first_file': args.first_file, 'second_file': args.second_file}
+
+
+def generate_diff(file_path_1, file_path_2):
+    file_1 = json.load(open(file_path_1))
+    file_2 = json.load(open(file_path_2))
+    unique_keys = sorted(set(file_1) | set(file_2))
     result = []
     for key in unique_keys:
         if key in file_1 and key not in file_2:
@@ -24,15 +31,15 @@ def generate_diff(file_1, file_2):
                 result.append(f"- {key}: {file_1[key]}")
                 result.append(f"+ {key}: {file_2[key]}")
     result_to_string = '\n'.join(result)
-    print(f'''{'{'}
-{result_to_string}
-{'}'}''')
+    return result_to_string
 
 
 def main():
-    file_1 = json.load(open(args.first_file))
-    file_2 = json.load(open(args.second_file))
-    generate_diff(file_1, file_2)
+    args = parse()
+    diff = generate_diff(args['first_file'], args['second_file'])
+    print(f'''{'{'}
+{diff}
+{'}'}''')
 
 
 if __name__ == '__main__':
