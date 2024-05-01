a6e777c5a4f3b662dc854f0539720034fa2cd161
Johnny32id <johnnyhandsome3232@gmail.com>
2024-05-02 00:02:34 +0300

    added plain tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,19 +2,23 @@ from gendiff.scripts.gendiff import generate_diff
 import os
 stylish_result = open(
     os.path.abspath('tests/fixtures/stylish_result.txt'), 'r').read()
+plain_result = open(
+    os.path.abspath('tests/fixtures/plain_result.txt'), 'r').read()
 
 
-def test_generate_diff_json_stylish():
-    path_to_file_1 = os.path.abspath('tests/fixtures/file1.json')
-    path_to_file_2 = os.path.abspath('tests/fixtures/file2.json')
-    assert generate_diff(path_to_file_1, path_to_file_2) == stylish_result
-    assert generate_diff(
-        path_to_file_1, path_to_file_2, 'stylish') == stylish_result
+def test_gendiff_stylish():
+    json_file_1 = os.path.abspath('tests/fixtures/file1.json')
+    json_file_2 = os.path.abspath('tests/fixtures/file2.json')
+    yaml_file_1 = os.path.abspath('tests/fixtures/file1.yaml')
+    yaml_file_2 = os.path.abspath('tests/fixtures/file2.yaml')
+    assert generate_diff(json_file_1, json_file_2, 'stylish') == stylish_result
+    assert generate_diff(yaml_file_1, yaml_file_2, 'stylish') == stylish_result
 
 
-def test_generate_diff_yaml_stylish():
-    path_to_file1 = os.path.abspath('tests/fixtures/file1.yaml')
-    path_to_file2 = os.path.abspath('tests/fixtures/file2.yaml')
-    assert generate_diff(path_to_file1, path_to_file2) == stylish_result
-    assert generate_diff(
-        path_to_file1, path_to_file2, 'stylish') == stylish_result
+def test_gendiff_plain():
+    json_file_1 = os.path.abspath('tests/fixtures/file1.json')
+    json_file_2 = os.path.abspath('tests/fixtures/file2.json')
+    yaml_file_1 = os.path.abspath('tests/fixtures/file1.yaml')
+    yaml_file_2 = os.path.abspath('tests/fixtures/file2.yaml')
+    assert generate_diff(json_file_1, json_file_2, 'plain') == plain_result
+    assert generate_diff(yaml_file_1, yaml_file_2, 'plain') == plain_result
