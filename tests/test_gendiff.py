ab07bcc4378e1b4dff32ddadac2d45e59eefd67d
Johnny32id <johnnyhandsome3232@gmail.com>
2024-05-01 22:13:51 +0300

    added stylish tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,15 +1,20 @@
 from gendiff.scripts.gendiff import generate_diff
 import os
-result = open(os.path.abspath('tests/fixtures/flat_result.txt'), 'r').read()
+stylish_result = open(
+    os.path.abspath('tests/fixtures/stylish_result.txt'), 'r').read()
 
 
-def test_generate_diff_json():
+def test_generate_diff_json_stylish():
     path_to_file_1 = os.path.abspath('tests/fixtures/file1.json')
     path_to_file_2 = os.path.abspath('tests/fixtures/file2.json')
-    assert generate_diff(path_to_file_1, path_to_file_2) == result
+    assert generate_diff(path_to_file_1, path_to_file_2) == stylish_result
+    assert generate_diff(
+        path_to_file_1, path_to_file_2, 'stylish') == stylish_result
 
 
-def test_generate_diff_yaml():
+def test_generate_diff_yaml_stylish():
     path_to_file1 = os.path.abspath('tests/fixtures/file1.yaml')
     path_to_file2 = os.path.abspath('tests/fixtures/file2.yaml')
-    assert generate_diff(path_to_file1, path_to_file2) == result
+    assert generate_diff(path_to_file1, path_to_file2) == stylish_result
+    assert generate_diff(
+        path_to_file1, path_to_file2, 'stylish') == stylish_result
