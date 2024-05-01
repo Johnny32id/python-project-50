67be6f8203ac52a18487eac1e274c673a7fcc6e7
Johnny32id <johnnyhandsome3232@gmail.com>
2024-05-02 00:31:29 +0300

    expanded the selection of formats

--- gendiff/scripts/formatters/formatters.py
+++ gendiff/scripts/formatters/formatters.py
@@ -2,6 +2,7 @@
 
 from .stylish import stylish
 from .plain import plain
+from .json import json_formatter
 
 
 def building_format(format, difference):
@@ -10,5 +11,7 @@ def building_format(format, difference):
             return stylish(difference)
         case 'plain':
             return plain(difference)
+        case 'json':
+            return json_formatter(difference)
         case _:
             raise TypeError(f'{format} is not available format')
