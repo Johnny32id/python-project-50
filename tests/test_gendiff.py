a75dab008f5d74db9a832dc7bc1705a14b059a9d
Johnny32id <johnnyhandsome3232@gmail.com>
2024-05-02 00:31:00 +0300

    added json tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,6 +4,8 @@ stylish_result = open(
     os.path.abspath('tests/fixtures/stylish_result.txt'), 'r').read()
 plain_result = open(
     os.path.abspath('tests/fixtures/plain_result.txt'), 'r').read()
+json_result = open(
+    os.path.abspath('tests/fixtures/json_result.json'), 'r').read()
 
 
 def test_gendiff_stylish():
@@ -22,3 +24,12 @@ def test_gendiff_plain():
     yaml_file_2 = os.path.abspath('tests/fixtures/file2.yaml')
     assert generate_diff(json_file_1, json_file_2, 'plain') == plain_result
     assert generate_diff(yaml_file_1, yaml_file_2, 'plain') == plain_result
+
+
+def test_gendiff_json():
+    json_file_1 = os.path.abspath('tests/fixtures/file1.json')
+    json_file_2 = os.path.abspath('tests/fixtures/file2.json')
+    yaml_file_1 = os.path.abspath('tests/fixtures/file1.yaml')
+    yaml_file_2 = os.path.abspath('tests/fixtures/file2.yaml')
+    assert generate_diff(json_file_1, json_file_2, 'json') == json_result
+    assert generate_diff(yaml_file_1, yaml_file_2, 'json') == json_result
