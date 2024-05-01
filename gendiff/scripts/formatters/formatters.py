14d6f56808d7c0839a2e4a012427c27cfe725647
Johnny32id <johnnyhandsome3232@gmail.com>
2024-05-02 00:01:58 +0300

    expanded the selection of formats

--- gendiff/scripts/formatters/formatters.py
+++ gendiff/scripts/formatters/formatters.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 from .stylish import stylish
+from .plain import plain
 
 
 def building_format(format, difference):
     match format:
         case 'stylish':
             return stylish(difference)
+        case 'plain':
+            return plain(difference)
         case _:
             raise TypeError(f'{format} is not available format')
