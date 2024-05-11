d64375f3ea87d830051e6e63697f0e01601d4fbc
Johnny32id <johnnyhandsome3232@gmail.com>
2024-05-11 13:08:01 +0300

    remove args_parse func

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,26 +1,13 @@
 #!/usr/bin/env python3
-import argparse
-from ..generagate_diff import generate_diff
-
-
-def args_parse():
-    parser = argparse.ArgumentParser(
-        description="Compares two configuration files and shows a difference.")
-    parser.add_argument("-f", "--format", type=str,
-                        default='stylish',
-                        help="set format of outputs")
-    parser.add_argument("first_file")
-    parser.add_argument("second_file")
-    args = parser.parse_args()
-    return {'first_file': args.first_file, 'second_file': args.second_file,
-            'format': args.format}
+from gendiff.args_parser import args_parse
+from gendiff.diff_builder import generate_diff
 
 
 def main():
     args = args_parse()
-    diff = generate_diff(args['first_file'],
-                         args['second_file'],
-                         args['format'])
+    diff = generate_diff(args.first_file,
+                         args.second_file,
+                         args.format)
     print(diff)
 
 
